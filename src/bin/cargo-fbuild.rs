48475cc12c48b20e1f50d6de9abaf694f22aa7f3
Alexander Lopatin <alopatindev@gmail.com>
2020-09-10 00:11:22 +0300

    Implement json-based solution

--- src/bin/cargo-fbuild.rs
+++ src/bin/cargo-fbuild.rs
@@ -1,14 +1,62 @@
-use cargo_fatal::execute_cargo_twice_and_exit;
-use std::env;
+use cargo_metadata::diagnostic::DiagnosticLevel;
+use cargo_metadata::Message;
 use std::iter;
+use std::process::{Command, Stdio};
+use terminal_size::{terminal_size, Width};
 
-const BUILD: &str = "build";
-const ENABLE_COLORS: &str = "--color=always";
+fn clear_current_line() {
+    if let Some((Width(width), _)) = terminal_size() {
+        let spaces = iter::repeat(' ').take(width as usize).collect::<String>();
+        print!("{}\r", spaces);
+    }
+}
 
 fn main() {
-    let first_arguments = iter::once(BUILD.to_owned()).chain(iter::once(ENABLE_COLORS.to_owned()));
-    let second_arguments = iter::once(BUILD.to_owned())
-        .chain(iter::once(ENABLE_COLORS.to_owned()))
-        .chain(env::args().skip(1));
-    execute_cargo_twice_and_exit(first_arguments, second_arguments);
+    let mut command = Command::new("cargo")
+        /*.args(&[
+            "test",
+            "--no-run",
+            "--message-format=json-diagnostic-rendered-ansi",
+        ])*/
+        .args(&["build", "--message-format=json-diagnostic-rendered-ansi"])
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let mut important_messages = Vec::new();
+    let mut boring_messages = Vec::new();
+
+    let reader = std::io::BufReader::new(command.stdout.take().unwrap());
+    for message in cargo_metadata::Message::parse_stream(reader) {
+        match message.unwrap() {
+            Message::CompilerMessage(msg) => {
+                if let Some(rendered) = msg.message.rendered {
+                    match msg.message.level {
+                        DiagnosticLevel::Error | DiagnosticLevel::Ice => {
+                            important_messages.push(rendered);
+                        }
+                        _ => {
+                            boring_messages.push(rendered);
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+
+    let limit = 1;
+    if important_messages.is_empty() {
+        for message in boring_messages.into_iter().take(limit) {
+            clear_current_line();
+            print!("{}", message);
+        }
+    } else {
+        for message in important_messages.into_iter().take(limit) {
+            clear_current_line();
+            print!("{}", message);
+        }
+    }
+
+    let _output = command.wait().expect("Couldn't get cargo's exit status");
 }
