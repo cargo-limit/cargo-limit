d7dd6de43b3b97c741201ce6d443be5c0ddc1164
Alexander Lopatin <alopatindev@gmail.com>
2020-09-20 16:45:06 +0300

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 mod flushing_writer;
+mod messages;
 mod options;
 
 use anyhow::{Context, Result};
-use cargo_metadata::{diagnostic::DiagnosticLevel, CompilerMessage, Message};
-use either::Either;
+use cargo_metadata::Message;
 use flushing_writer::FlushingWriter;
-use itertools::Itertools;
+use messages::{process_messages, read_raw_messages, ParsedMessages};
 use options::Options;
 use std::{
     env,
-    io::{self, BufRead, BufReader, Cursor},
+    io::{self, BufReader},
     path::PathBuf,
     process::{Command, Stdio},
 };
@@ -17,19 +17,11 @@ use std::{
 const CARGO_EXECUTABLE: &str = "cargo";
 const CARGO_ENV_VAR: &str = "CARGO";
 const NO_EXIT_CODE: i32 = 127;
-const BUILD_FINISHED_MESSAGE: &str = r#""build-finished""#;
 const ADDITIONAL_ENVIRONMENT_VARIABLES: &str =
     "Additional environment variables:\n    CARGO_LIMIT         Limit compiler messages number (0 \
      means no limit, which is default)\n    CARGO_ASC           Show compiler messages in \
      ascending order (false is default)\n    CARGO_FORCE_WARN    Show warnings even if errors \
      still exist (false is default)";
-
-#[derive(Default)]
-struct ParsedMessages {
-    internal_compiler_errors: Vec<CompilerMessage>,
-    errors: Vec<CompilerMessage>,
-    non_errors: Vec<CompilerMessage>,
-}
 
 pub fn run_cargo_filtered(cargo_command: &str) -> Result<i32> {
     let parsed_args = Options::from_args_and_vars(cargo_command)?;
@@ -74,86 +66,4 @@ pub fn run_cargo_filtered(cargo_command: &str) -> Result<i32> {
 
     let exit_code = command.wait()?.code().unwrap_or(NO_EXIT_CODE);
     Ok(exit_code)
-}
-
-impl ParsedMessages {
-    fn parse(raw_messages: Vec<u8>) -> Result<Self> {
-        let mut result = ParsedMessages::default();
-
-        for message in Message::parse_stream(Cursor::new(raw_messages)) {
-            if let Message::CompilerMessage(compiler_message) = message? {
-                match compiler_message.message.level {
-                    DiagnosticLevel::Ice => result.internal_compiler_errors.push(compiler_message),
-                    DiagnosticLevel::Error => result.errors.push(compiler_message),
-                    _ => result.non_errors.push(compiler_message),
-                }
-            }
-        }
-
-        Ok(result)
-    }
-}
-
-fn process_messages(
-    parsed_messages: ParsedMessages,
-    parsed_args: &Options,
-) -> impl Iterator<Item = Message> {
-    let messages = if parsed_args.show_warnings_if_errors_exist {
-        Either::Left(
-            parsed_messages
-                .internal_compiler_errors
-                .into_iter()
-                .chain(parsed_messages.errors.into_iter())
-                .chain(parsed_messages.non_errors.into_iter()),
-        )
-    } else {
-        let has_any_errors = !parsed_messages.internal_compiler_errors.is_empty()
-            || !parsed_messages.errors.is_empty();
-        let messages = if has_any_errors {
-            Either::Left(
-                parsed_messages
-                    .internal_compiler_errors
-                    .into_iter()
-                    .chain(parsed_messages.errors.into_iter()),
-            )
-        } else {
-            Either::Right(parsed_messages.non_errors.into_iter())
-        };
-        Either::Right(messages)
-    };
-
-    let messages = messages.unique();
-
-    let limit_messages = parsed_args.limit_messages;
-    let no_limit = limit_messages == 0;
-    let messages = if no_limit {
-        Either::Left(messages)
-    } else {
-        Either::Right(messages.take(limit_messages))
-    };
-
-    let messages = messages.collect::<Vec<_>>().into_iter();
-    let messages = if parsed_args.ascending_messages_order {
-        Either::Left(messages)
-    } else {
-        Either::Right(messages.rev())
-    };
-
-    messages.map(Message::CompilerMessage)
-}
-
-fn read_raw_messages<R: io::Read>(reader: &mut BufReader<R>) -> Result<Vec<u8>> {
-    let mut line = String::new();
-    let mut raw_messages = Vec::new();
-
-    loop {
-        let len = reader.read_line(&mut line)?;
-        raw_messages.extend(line.as_bytes());
-        if len == 0 || line.contains(BUILD_FINISHED_MESSAGE) {
-            break;
-        }
-        line.clear();
-    }
-
-    Ok(raw_messages)
 }
---
+++ src/messages.rs
@@ -0,0 +1,97 @@
+use crate::options::Options;
+use anyhow::Result;
+use cargo_metadata::{diagnostic::DiagnosticLevel, CompilerMessage, Message};
+use either::Either;
+use itertools::Itertools;
+use std::io::{self, BufRead, BufReader, Cursor};
+
+const BUILD_FINISHED_MESSAGE: &str = r#""build-finished""#;
+
+#[derive(Default)]
+pub struct ParsedMessages {
+    internal_compiler_errors: Vec<CompilerMessage>,
+    errors: Vec<CompilerMessage>,
+    non_errors: Vec<CompilerMessage>,
+}
+
+impl ParsedMessages {
+    pub fn parse(raw_messages: Vec<u8>) -> Result<Self> {
+        let mut result = ParsedMessages::default();
+
+        for message in Message::parse_stream(Cursor::new(raw_messages)) {
+            if let Message::CompilerMessage(compiler_message) = message? {
+                match compiler_message.message.level {
+                    DiagnosticLevel::Ice => result.internal_compiler_errors.push(compiler_message),
+                    DiagnosticLevel::Error => result.errors.push(compiler_message),
+                    _ => result.non_errors.push(compiler_message),
+                }
+            }
+        }
+
+        Ok(result)
+    }
+}
+
+pub fn process_messages(
+    parsed_messages: ParsedMessages,
+    parsed_args: &Options,
+) -> impl Iterator<Item = Message> {
+    let messages = if parsed_args.show_warnings_if_errors_exist {
+        Either::Left(
+            parsed_messages
+                .internal_compiler_errors
+                .into_iter()
+                .chain(parsed_messages.errors.into_iter())
+                .chain(parsed_messages.non_errors.into_iter()),
+        )
+    } else {
+        let has_any_errors = !parsed_messages.internal_compiler_errors.is_empty()
+            || !parsed_messages.errors.is_empty();
+        let messages = if has_any_errors {
+            Either::Left(
+                parsed_messages
+                    .internal_compiler_errors
+                    .into_iter()
+                    .chain(parsed_messages.errors.into_iter()),
+            )
+        } else {
+            Either::Right(parsed_messages.non_errors.into_iter())
+        };
+        Either::Right(messages)
+    };
+
+    let messages = messages.unique();
+
+    let limit_messages = parsed_args.limit_messages;
+    let no_limit = limit_messages == 0;
+    let messages = if no_limit {
+        Either::Left(messages)
+    } else {
+        Either::Right(messages.take(limit_messages))
+    };
+
+    let messages = messages.collect::<Vec<_>>().into_iter();
+    let messages = if parsed_args.ascending_messages_order {
+        Either::Left(messages)
+    } else {
+        Either::Right(messages.rev())
+    };
+
+    messages.map(Message::CompilerMessage)
+}
+
+pub fn read_raw_messages<R: io::Read>(reader: &mut BufReader<R>) -> Result<Vec<u8>> {
+    let mut line = String::new();
+    let mut raw_messages = Vec::new();
+
+    loop {
+        let len = reader.read_line(&mut line)?;
+        raw_messages.extend(line.as_bytes());
+        if len == 0 || line.contains(BUILD_FINISHED_MESSAGE) {
+            break;
+        }
+        line.clear();
+    }
+
+    Ok(raw_messages)
+}
