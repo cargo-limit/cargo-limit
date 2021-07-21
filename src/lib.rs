b76fcb7c4fe14c56b05eb5e78ad6da7333d2bd4e
Alexander Lopatin <alopatindev@gmail.com>
2021-07-22 01:24:28 +0300

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@ mod options;
 mod process;
 
 use anyhow::{Context, Result};
-use cargo_metadata::{Message, MetadataCommand};
+use cargo_metadata::{diagnostic::DiagnosticSpan, Message, MetadataCommand};
 use io::Buffers;
 use messages::{process_messages, ParsedMessages, ProcessedMessages};
 use options::Options;
 use std::{
     env, fmt,
     io::Write,
-    path::PathBuf,
+    path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 
@@ -50,46 +50,7 @@ pub fn run_cargo_filtered(cargo_command: &str) -> Result<i32> {
     let version = parsed_args.version;
 
     if !help && !version {
-        let parsed_messages =
-            ParsedMessages::parse(buffers.child_stdout_reader_mut(), cargo_pid, &parsed_args)?;
-        let ProcessedMessages {
-            messages,
-            spans_in_consistent_order,
-        } = process_messages(parsed_messages, &parsed_args, &workspace_root)?;
-        let processed_messages = messages.into_iter();
-
-        if parsed_args.json_message_format {
-            for message in processed_messages {
-                buffers.writeln_to_stdout(serde_json::to_string(&message)?)?;
-            }
-        } else {
-            for message in processed_messages.filter_map(|message| match message {
-                Message::CompilerMessage(compiler_message) => compiler_message.message.rendered,
-                _ => None,
-            }) {
-                buffers.write_to_stderr(message)?;
-            }
-        }
-
-        let open_in_external_application = parsed_args.open_in_external_application;
-        if !open_in_external_application.is_empty() {
-            let mut args = Vec::new();
-            for span in spans_in_consistent_order.into_iter() {
-                args.push(format!(
-                    "{}:{}:{}",
-                    span.file_name, span.line_start, span.column_start
-                ));
-            }
-            if !args.is_empty() {
-                let error_text = failed_to_execute_error_text(&open_in_external_application);
-                let output = Command::new(open_in_external_application)
-                    .args(args)
-                    .output()
-                    .context(error_text)?;
-                buffers.write_all_to_stderr(&output.stdout)?;
-                buffers.write_all_to_stderr(&output.stderr)?;
-            }
-        }
+        parse_and_process_messages(&mut buffers, cargo_pid, &parsed_args, workspace_root)?;
     }
 
     buffers.copy_from_child_stdout_reader_to_stdout_writer()?;
@@ -103,6 +64,63 @@ pub fn run_cargo_filtered(cargo_command: &str) -> Result<i32> {
     // TODO: process messages again
     //buffers.copy_from_child_reader_to_stdout_writer()?;
     Ok(exit_code)
+}
+
+fn parse_and_process_messages(
+    buffers: &mut Buffers,
+    cargo_pid: u32,
+    parsed_args: &Options,
+    workspace_root: PathBuf,
+) -> Result<()> {
+    let parsed_messages =
+        ParsedMessages::parse(buffers.child_stdout_reader_mut(), cargo_pid, parsed_args)?;
+    let ProcessedMessages {
+        messages,
+        spans_in_consistent_order,
+    } = process_messages(parsed_messages, &parsed_args, &workspace_root)?;
+    let processed_messages = messages.into_iter();
+
+    if parsed_args.json_message_format {
+        for message in processed_messages {
+            buffers.writeln_to_stdout(serde_json::to_string(&message)?)?;
+        }
+    } else {
+        for message in processed_messages.filter_map(|message| match message {
+            Message::CompilerMessage(compiler_message) => compiler_message.message.rendered,
+            _ => None,
+        }) {
+            buffers.write_to_stderr(message)?;
+        }
+    }
+
+    open_in_external_application_for_affected_files(buffers, spans_in_consistent_order, parsed_args)
+}
+
+fn open_in_external_application_for_affected_files(
+    buffers: &mut Buffers,
+    spans_in_consistent_order: Vec<DiagnosticSpan>,
+    parsed_args: &Options,
+) -> Result<()> {
+    let open_in_external_application = &parsed_args.open_in_external_application;
+    if !open_in_external_application.is_empty() {
+        let mut args = Vec::new();
+        for span in spans_in_consistent_order.into_iter() {
+            args.push(format!(
+                "{}:{}:{}",
+                span.file_name, span.line_start, span.column_start
+            ));
+        }
+        if !args.is_empty() {
+            let error_text = failed_to_execute_error_text(open_in_external_application);
+            let output = Command::new(open_in_external_application)
+                .args(args)
+                .output()
+                .context(error_text)?;
+            buffers.write_all_to_stderr(&output.stdout)?;
+            buffers.write_all_to_stderr(&output.stderr)?;
+        }
+    }
+    Ok(())
 }
 
 fn failed_to_execute_error_text<T: fmt::Debug>(program: T) -> String {
