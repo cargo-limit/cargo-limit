73dfb02562dd6d46947232960b5735e1c48017ab
Alexander Lopatin <alopatindev@gmail.com>
2022-09-17 02:02:07 +0800

    Don't deduplicate by file

--- src/messages.rs
+++ src/messages.rs
@@ -7,7 +7,7 @@ use cargo_metadata::{
 use getset::CopyGetters;
 use itertools::{Either, Itertools};
 use process::CargoProcess;
-use std::{collections::HashSet, path::Path, time::Duration};
+use std::{path::Path, time::Duration};
 
 #[derive(Default, CopyGetters, Debug)]
 pub struct Messages {
@@ -259,7 +259,7 @@ impl TransformedMessages {
         options: &Options,
         workspace_root: &Path,
     ) -> Vec<SourceFile> {
-        let spans_and_messages = messages
+        messages
             .iter()
             .filter(|message| {
                 if options.open_in_external_app_on_warnings() {
@@ -280,22 +280,14 @@ impl TransformedMessages {
                     .cloned()
                     .map(move |span| (span, message))
             })
-            .map(|(span, message)| (Self::find_leaf_project_expansion(span), &message.message));
-
-        let mut source_files_in_consistent_order = Vec::new();
-        let mut used_file_names = HashSet::new();
-        for (span, message) in spans_and_messages {
-            if !used_file_names.contains(&span.file_name) {
-                used_file_names.insert(span.file_name.clone());
-                source_files_in_consistent_order.push(SourceFile::new(
-                    span,
-                    message,
+            .map(|(span, message)| {
+                SourceFile::new(
+                    Self::find_leaf_project_expansion(span),
+                    &message.message,
                     workspace_root,
-                ));
-            }
-        }
-
-        source_files_in_consistent_order
+                )
+            })
+            .collect()
     }
 
     fn find_leaf_project_expansion(mut span: DiagnosticSpan) -> DiagnosticSpan {
