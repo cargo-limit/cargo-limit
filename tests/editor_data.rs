476e481116e33861350189ba775a43539a75e3b0
Alexander Lopatin <alopatindev@codonaft.com>
2025-08-31 15:17:31 +0400

    Cleanup

--- tests/editor_data.rs
+++ tests/editor_data.rs
@@ -7,8 +7,6 @@ use std::{
     process::Command,
 };
 
-// TODO: install xq or jaq?
-
 #[test]
 fn bugs() -> anyhow::Result<()> {
     check("bugs")
@@ -20,27 +18,22 @@ fn typos() -> anyhow::Result<()> {
 }
 
 fn check(project: &str) -> anyhow::Result<()> {
-    dbg!("check_editor_data 1");
     let workspace_root = Path::new(env!("CARGO_MANIFEST_DIR"));
     let project_dir = workspace_root.join("tests/stubs").join(project);
-    dbg!("check_editor_data 2");
     let target_dir = env::current_exe()?
         .parent()
         .context("parent")?
         .join("../../release");
-    dbg!("check_editor_data 2.1", &target_dir);
     let output = Command::new(target_dir.join("cargo-llcheck"))
         .env("CARGO_EDITOR", "xq")
         .current_dir(&project_dir)
         .output()?;
-    dbg!("check_editor_data 3");
     assert!(!output.status.success());
     let data: EditorData = serde_json::from_slice(&output.stdout)?;
     dbg!(&data); // TODO
     assert_eq!(data.workspace_root, project_dir);
     assert!(!data.locations.is_empty());
 
-    dbg!("check_editor_data 4");
     let mut current_line = None;
     let mut current_path = None;
     let mut visited_paths = HashSet::<PathBuf>::default();
@@ -56,6 +49,5 @@ fn check(project: &str) -> anyhow::Result<()> {
         assert_eq!(current_path, Some(i.path));
         current_line = Some(i.line);
     }
-    dbg!("check_editor_data 5");
     Ok(())
 }
