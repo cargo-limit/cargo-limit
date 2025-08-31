b8c0f41075eb4050155d04ccf18db7120a4aaded
Alexander Lopatin <alopatindev@codonaft.com>
2025-08-31 14:15:23 +0400

    Debug

--- tests/editor_data.rs
+++ tests/editor_data.rs
@@ -8,18 +8,22 @@ use std::{
 // TODO: install xq or jaq?
 
 fn check_editor_data(project: &str) -> anyhow::Result<()> {
+    dbg!("check_editor_data 1");
     let workspace_root = Path::new(env!("CARGO_MANIFEST_DIR"));
     let project_dir = workspace_root.join("tests/stubs").join(project);
+    dbg!("check_editor_data 2");
     let output = Command::new(workspace_root.join("target/release/cargo-llcheck"))
         .env("CARGO_EDITOR", "cat")
         .current_dir(&project_dir)
         .output()?;
+    dbg!("check_editor_data 3");
     assert!(!output.status.success());
     let data: EditorData = serde_json::from_slice(&output.stdout)?;
     dbg!(&data); // TODO
     assert_eq!(data.workspace_root, project_dir);
     assert!(!data.locations.is_empty());
 
+    dbg!("check_editor_data 4");
     let mut current_line = None;
     let mut current_path = None;
     let mut visited_paths = HashSet::<PathBuf>::default();
@@ -35,6 +39,7 @@ fn check_editor_data(project: &str) -> anyhow::Result<()> {
         assert_eq!(current_path, Some(i.path));
         current_line = Some(i.line);
     }
+    dbg!("check_editor_data 5");
     Ok(())
 }
 
