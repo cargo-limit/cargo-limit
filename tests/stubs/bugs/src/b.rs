21a62363160c3dcda686529a249354e27e4348de
Alexander Lopatin <alopatindev@codonaft.com>
2025-08-31 13:29:43 +0400

    Update

--- tests/stubs/bugs/src/b.rs
+++ tests/stubs/bugs/src/b.rs
@@ -1,27 +1,27 @@
 use std::time::Duration;
 
 #[derive(Debug, PartialEq)]
-struct Options {
-    a: Duration,
+struct B {
+    b: Duration,
 }
 
-impl Default for Options {
+impl Default for B {
     fn default() -> Self {
         Self {
-            a: Some(Duration::from_secs(1)), // NOTE
+            b: Some(Duration::from_secs(1)), // NOTE
         }
     }
 }
 
-impl Options {
+impl B {
     fn f() {
-        let mut result = Self::default();
+        let mut result = B::default();
         let mut seconds = result
-            .a
+            .b
             .map(Duration::as_secs) // NOTE
             .unwrap_or(0);
         let duration = Duration::from_secs(seconds);
-        result.a = if duration > Duration::from_secs(0) {
+        result.b = if duration > Duration::from_secs(0) {
             Some(duration) // NOTE
         } else {
             None // NOTE
