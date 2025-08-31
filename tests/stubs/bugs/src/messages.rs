95e7683ebd3eb74ceb725a4764cc3c9d95f0d080
Alexander Lopatin <alopatindev@codonaft.com>
2025-08-31 13:07:09 +0400

    Update

--- tests/stubs/bugs/src/messages.rs
+++ tests/stubs/bugs/src/messages.rs
@@ -1,13 +1,3 @@
-use crate::{io::Buffers, models::Location, options::Options, process};
-use anyhow::Result;
-use cargo_metadata::{
-    diagnostic::{DiagnosticLevel, DiagnosticSpan},
-    CompilerMessage, Message,
-};
-use itertools::{Either, Itertools};
-use process::CargoProcess;
-use std::path::Path;
-
 #[derive(Default, Debug)]
 pub struct Messages {
     pub child_killed: bool,
