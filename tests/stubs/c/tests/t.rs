44595f5db185037ad274439319fe147915476558
Alexander Lopatin <alopatindev@codonaft.com>
2025-09-01 07:53:46 +0400

    Update

--- tests/stubs/c/tests/t.rs
+++ tests/stubs/c/tests/t.rs
@@ -6,10 +6,10 @@ fn c() {
     let output = Command::new("").output().unwrap();
     let data: A = serde_json::from_slice(&output.stdout).unwrap();
     let mut current = None;
-    let mut visited = HashSet::<PathBuf>::default();
+    let mut v = HashSet::<PathBuf>::default();
     for i in data.b {
-        if !visited.contains(&i.c) {
-            visited.insert(i.c);
+        if !v.contains(&i.c) {
+            v.insert(i.c);
             current = Some(i.c);
         }
         assert_eq!(current, Some(i.c));
