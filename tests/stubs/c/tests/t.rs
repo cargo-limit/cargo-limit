a282e909d6b00a064e54593564e13eea0fa87406
Alexander Lopatin <alopatindev@codonaft.com>
2025-09-01 07:57:49 +0400

    Update

--- tests/stubs/c/tests/t.rs
+++ tests/stubs/c/tests/t.rs
@@ -5,13 +5,12 @@ use std::{collections::HashSet, path::PathBuf, process::Command};
 fn c() {
     let output = Command::new("").output().unwrap();
     let a: A = serde_json::from_slice(&output.stdout).unwrap();
-    let mut current = None;
     let mut v = HashSet::<PathBuf>::default();
     for i in a.b {
         if !v.contains(&i.c) {
             v.insert(i.c);
-            current = Some(i.c);
+            v.insert(i.c);
         }
-        assert_eq!(current, Some(i.c));
+        v.insert(i.c);
     }
 }
